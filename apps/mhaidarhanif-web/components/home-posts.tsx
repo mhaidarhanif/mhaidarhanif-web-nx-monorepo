f84a637d1868f2ca0f49a6919aa5da2dd3939d99
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-09 06:26:51 +0700

    feat(web): link blog posts

--- apps/mhaidarhanif-web/components/home-posts.tsx
+++ apps/mhaidarhanif-web/components/home-posts.tsx
@@ -1,3 +1,4 @@
+import NextLink from 'next/link'
 import NextImage from 'next/image'
 import { Grid, Card, Heading, Box, Text } from 'theme-ui'
 
@@ -33,44 +34,54 @@ export const HomePosts = () => {
       <Heading as="h1">My blog posts</Heading>
       <Grid columns={1} gap={4} mt={4}>
         {blogPosts.map((post) => {
-          return (
-            <Card
-              key={post.slug}
-              sx={{
-                display: 'flex',
-                margin: '0 auto',
-                width: '100%',
-                flexDirection: ['column', 'row'],
-              }}
-            >
-              <Box className="next-image-box">
-                <NextImage
-                  src={post.imageUrl}
-                  alt={post.title}
-                  width={150}
-                  height={150}
-                  layout="fixed"
-                />
-              </Box>
-              <Box
-                sx={{
-                  mt: [3, 0],
-                  ml: [0, 5],
-                }}
-              >
-                <code>/{post.slug}</code>
-                <Heading as="h1" mt={2}>
-                  {post.title}
-                </Heading>
-                <Text as="p" mt={3}>
-                  {post.description}
-                </Text>
-              </Box>
-            </Card>
-          )
+          return <BlogPostCard key={post.slug} post={post} />
         })}
       </Grid>
     </Box>
+  )
+}
+
+export const BlogPostCard = ({ post }) => {
+  return (
+    <NextLink href={`/blog/${post.slug}`}>
+      <Card
+        sx={{
+          display: 'flex',
+          margin: '0 auto',
+          width: '100%',
+          flexDirection: ['column', 'row'],
+          '&:hover': {
+            cursor: 'pointer',
+            transition: 'all 0.3s ease-in-out',
+            transform: 'scale(1.02)',
+          },
+        }}
+      >
+        <Box className="next-image-box">
+          <NextImage
+            src={post.imageUrl}
+            alt={post.title}
+            width={150}
+            height={150}
+            layout="fixed"
+          />
+        </Box>
+        <Box
+          sx={{
+            mt: [3, 0],
+            ml: [0, 5],
+          }}
+        >
+          <code>/{post.slug}</code>
+          <Heading as="h1" mt={2}>
+            {post.title}
+          </Heading>
+          <Text as="p" mt={3}>
+            {post.description}
+          </Text>
+        </Box>
+      </Card>
+    </NextLink>
   )
 }
 
