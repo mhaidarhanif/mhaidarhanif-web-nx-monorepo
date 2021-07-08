187f7ae5cb4de7a2e45c405f1a006730bab3b375
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-09 00:46:40 +0700

    feat(web): clean up contents

--- apps/mhaidarhanif-web/pages/_app.tsx
+++ apps/mhaidarhanif-web/pages/_app.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
+import React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import './styles.css'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to mhaidarhanif-web!</title>
+        <title>M Haidar Hanif</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to mhaidarhanif-web!</h1>
+
+      <div>
+        <header>
+          <h1>M Haidar Hanif</h1>
         </header>
+
         <main>
           <Component {...pageProps} />
         </main>
       </div>
     </>
-  );
+  )
 }
 
-export default CustomApp;
+export default CustomApp
